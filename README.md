7a1d249339f0a2586ee497a65ff7c96639a6e8bc
Yni-Viar <yniviar@outlook.com>
2026-06-18 18:03:22 +0300

    Deprecate this library and lower version of Godot

--- README.md
+++ README.md
@@ -1,5 +1,3 @@
+# ⚠️ DEPRECATED, since Godot 4.7 has this virtual joystick built-in!
+
 # Godot 4.7 touch/virtual joystick ported to Godot 4.6 and earlier.
-
-As of February 2026 Godot 4.7 is still in dev, why not to port this new feature to Godot 4.6?
-
-*⚠️  When Godot 4.7 come out, this repo will be deprecated.*
--- sample/test.gd
+++ sample/test.gd
@@ -5,12 +5,9 @@ extends Control
 func _ready() -> void:
 	pass # Replace with function body.
 
-func _input(event: InputEvent) -> void:
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _physics_process(delta: float) -> void:
 	if Input.is_action_pressed("ui_right"):
 		$Sprite2D.rotate(PI / 360)
 	elif Input.is_action_pressed("ui_left"):
 		$Sprite2D.rotate(-PI / 360)
-
-# Called every frame. 'delta' is the elapsed time since the previous frame.
-func _process(delta: float) -> void:
-	pass
